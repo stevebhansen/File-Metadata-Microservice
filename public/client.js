33b9722a9a013a9366f681b0223fc99b4ea86fec
System <none>
2017-08-13 23:53:20 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -16,7 +16,7 @@ $(function() {
   $('form').submit(function(event) {
     event.preventDefault();
     var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
+    $.post('/upload?' + $.param({dream: dream}), function() {
       $('<li></li>').text(dream).appendTo('ul#dreams');
       $('input').val('');
       $('input').focus();
