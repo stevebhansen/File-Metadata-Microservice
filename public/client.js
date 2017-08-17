0059868733082b83d0c4f921f79ab89cfa431dcc
System <none>
2017-08-17 02:16:30 +0000

    Checkpoint

--- public/client.js
+++ public/client.js
@@ -6,10 +6,9 @@
 
 $('form').submit(function(event) {
   event.preventDefault();
+  var file = $('form').prop('file');
   console.log('hello world :o');
   event.preventDefault();
-  $.post('https://smooth-name.glitch.me/upload', function() {
-    $('#form-file').submit();
-  });
+  $.post('https://smooth-name.glitch.me/upload', { s: file });
 });
 
